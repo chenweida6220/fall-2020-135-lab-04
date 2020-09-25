918ce01e14aeff0a3f441c151842053c3a538c6c
Wei Da Chen <weida.wdc@gmail.com>
2020-09-25 10:01:48 -0400

    revised input

--- main.cpp
+++ main.cpp
@@ -5,56 +5,37 @@ using namespace std;
 
 int main() {
   // Task A
-  int width;
-  cout << "Input width: ";
-  cin >> width;
-  int height;
-  cout << "Input height: ";
-  cin >> height;
-  cout << endl;
-  cout << box(width,height) << endl;
+  string taskA;
+  taskA = box(7,4);
+  cout << taskA << "----------" << endl << endl;
 
   // Task B
-  cout << "Input width: ";
-  cin >> width;
-  cout << "Input height: ";
-  cin >> height;
-  cout << endl;
-  cout << checkerboard(width,height) << endl;
+  string taskB;
+  taskB = checkerboard(11,6);
+  cout << taskB << "----------" << endl << endl;
 
   // Task C
-  int size;
-  cout << "Input size: ";
-  cin >> size;
-  cout << endl;
-  cout << cross(size) << endl;
+  string taskC;
+  taskC = cross(8);
+  cout << taskC << "----------" << endl << endl;
 
   // Task D
-  int length;
-  cout << "Input side length: ";
-  cin >> length;
-  cout << endl;
-  cout << lowerTriangle(length) << endl;
+  string taskD;
+  taskD = lowerTriangle(6);
+  cout << taskD << "----------" << endl << endl;
 
   // Task E
-  cout << "Input side length: ";
-  cin >> length;
-  cout << endl;
-  cout << upperTriangle(length) << endl;
+  string taskE;
+  taskE = upperTriangle(5);
+  cout << taskE << "----------" << endl << endl;
 
   // Task F
-  cout << "Input width: ";
-  cin >> width;
-  cout << "Input length: ";
-  cin >> length;
-  cout << endl;
-  cout << trapezoid(width, length) << endl;
+  string taskF;
+  taskF = trapezoid(12, 5);
+  cout << taskF << "----------" << endl << endl;
 
   // Task G
-  cout << "Input width: ";
-  cin >> width;
-  cout << "Input height: ";
-  cin >> height;
-  cout << endl;
-  cout << checkerboard3x3(width, height) << endl;
+  string taskG;
+  taskG = checkerboard3x3(16, 11);
+  cout << taskG << "----------" << endl << endl;
 }
